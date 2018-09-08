2aecaa527b562e70bebae115df5cec1a3d6e7ecc
JinLisek <tastyramen@gmail.com>
2018-09-08 11:30:16 +0200

    Added first version of tanks game: tank moves

---
+++ Scripts/PlayerTank.gd
@@ -0,0 +1,19 @@
+extends "res://Scripts/Tank.gd"
+
+func control(delta):
+	$Turret.look_at(get_global_mouse_position())
+	
+	var rotation_dir = 0
+	
+	if Input.is_action_pressed("turn_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("turn_left"):
+		rotation_dir -= 1
+
+	rotation += rotation_dir * rotation_speed * delta
+	
+	velocity = Vector2()
+	if Input.is_action_pressed("forward"):
+		velocity = Vector2(movement_speed, 0).rotated(rotation)
+	if Input.is_action_pressed("backward"):
+		velocity = Vector2(-movement_speed / 2, 0).rotated(rotation)
